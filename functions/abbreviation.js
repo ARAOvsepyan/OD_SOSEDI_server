b32097fb008dd6d79f6394b3945d43b2d4367cbc
Ara Ovsepyan <ara07.99@icloud.com>
2022-06-02 05:42:45 +0300

    Update abbreviation.js

--- functions/abbreviation.js
+++ functions/abbreviation.js
@@ -1,30 +1,3 @@
-function makeAbbreviation(data){
-    if (data =='Центральный')
-        return (data = 'ЦАО')
-    else if (data == 'Северный')
-        return (data = 'САО')
-    else if (data == 'Северо-восточный')
-        return (data = 'СВАО')
-    else if (data == 'Восточный')
-        return (data = 'ВАО')
-    else if (data == 'Юго-восточный')
-        return (data = 'ЮВАО')
-    else if (data == 'Южный')
-        return (data = 'ЮАО')
-    else if (data == 'Юго-западный')
-        return (data = 'ЮЗАО')
-    else if (data == 'Западный')
-        return (data = 'ЗАО')
-    else if (data == 'Северо-западный')
-        return (data = 'СЗАО')
-    else if (data == 'Зеленоградский')
-        return (data = 'ЗелАО')
-    else if ((data == 'Новомосковский') || (data == 'Троицкий'))
-        return (data = 'ТиНАО')
-    else
-        return data
-}
-
 class Abbreviation {    
     getAdress(data) {
         let arr = []
@@ -36,7 +9,7 @@ class Abbreviation {
             arr.push('Москва')
     
         // АО
-        arr.push(makeAbbreviation(data.city_area));
+        arr.push(data.city_area);
     
         // Район
         if (data.area != null) {  
